3f4a8c65477b0248a9e97a3b90231f253e3ad648
MCausc78 <mcausc78@gmail.com>
2024-07-11 18:28:23 +0300

    Cleanup TODO generator

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+- https://github.com/MCausc78/pyvolt/blob/ready/pyvolt/discovery.py#L194C7-L194C38 TODO: Add `Theme.apply` method.
+- https://github.com/MCausc78/pyvolt/blob/ready/pyvolt/events.py#L261C15-L261C80 TODO: What we should do in case of mentions? This solution sucks.
+- https://github.com/MCausc78/pyvolt/blob/ready/pyvolt/http.py#L201C19-L201C38 TODO: Handle 10053?
+- https://github.com/MCausc78/pyvolt/blob/ready/pyvolt/shard.py#L264C19-L264C38 TODO: Handle 10053?
--- gen_todo.py
+++ gen_todo.py
@@ -1,20 +1,54 @@
 from glob import glob
 import re
+import subprocess
+import sys
+
+
+def get_branch() -> str:
+    proc = subprocess.run("git status", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.check_returncode()
+
+    RE_BRANCH = re.compile(rb"On branch ([0-9A-Za-z/]+)\n", re.MULTILINE)
+
+    match = RE_BRANCH.findall(proc.stdout)
+    if not match:
+        print(f"Unable to get git branch! Stdout:")
+        sys.stdout.write(proc.stdout.decode())
+        sys.exit(1)
+
+    return match[0].decode()
 
 
 def main():
+    branch = get_branch()
+
     RE_TODO = re.compile(r"TODO: (.*)")
 
-    todos = []
+    todos = {}
     for filename in glob("./pyvolt/**.py", recursive=True):
         with open(filename, "r") as fp:
-            for line in fp:
-                todos.extend(RE_TODO.findall(line))
+            todos.setdefault(filename, [])
+            for i, line in enumerate(fp):
+                match = RE_TODO.search(line)
+                if match:
+                    span = match.span()
+                    span = (span[0] + 1, min(len(line), span[1] + 1))
+
+                    todos[filename].append((i + 1, span, match[0]))
 
     with open("TODO.md", "w") as todo_md:
         todo_md.write("# TODO\n\n")
-        for todo in todos:
-            todo_md.write(f"- {todo}\n")
+        for file, items in todos.items():
+            if not items:
+                continue
+
+            for item in items:
+                file = file[file.find("pyvolt") :].replace("\\", "/")
+                span = item[1]
+                fragment = f"L{item[0]}C{span[0]}-L{item[0]}C{span[1]}"
+                todo_md.write(
+                    f"- https://github.com/MCausc78/pyvolt/blob/{branch}/{file}#{fragment} {item[2]}\n"
+                )
 
 
 if __name__ == "__main__":
