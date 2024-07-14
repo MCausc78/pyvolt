6a3388f19773bcf22c1703d6348298d107249f44
MCausc78 <mcausc78@gmail.com>
2024-07-14 18:57:20 +0300

    Add DM/GDM channels parsing benchmark

--- bench/__main__.py
+++ bench/__main__.py
@@ -1,14 +1,20 @@
 import asyncio
 
-from .bench_channel import bench_channels
+from .bench_channel import bench_dm_channels, bench_group_channels, bench_text_channels
 from .bench_member import bench_members
 from .bench_message import bench_messages
 from .bench_server import bench_servers
 from .bench_user import bench_users
 
 async def main():
-    print('Benchmarking Channel parsing.')
-    await bench_channels()
+    print('Benchmarking DMChannel parsing.')
+    await bench_dm_channels()
+
+    print('Benchmarking GroupChannel parsing.')
+    await bench_group_channels()
+
+    print('Benchmarking TextChannel parsing.')
+    await bench_text_channels()
 
     print('Benchmarking Member parsing.')
     await bench_members()
--- bench/bench_channel.py
+++ bench/bench_channel.py
@@ -12,7 +12,112 @@ import typing
 import voltage
 import voltage.internals
 
-async def bench_channels():
+async def bench_dm_channels():
+    with open('./test/data/channels/dm.json', 'r') as fp:
+        payload = json.load(fp)
+    
+    state = pyvolt.State()
+    parser = pyvolt.Parser(state)
+    state.setup(parser=parser)
+
+    def using_pyvolt():
+        return parser.parse_direct_message_channel(payload)
+
+    import aiohttp
+    session = aiohttp.ClientSession()
+
+    api_info: typing.Any = {
+        'features': {
+            'autumn': {
+                'url': 'https://autumn.revolt.chat/'
+            }
+        }
+    }
+    rpy_http = revolt.http.HttpClient(
+        session,
+        '',
+        'https://api.revolt.chat/',
+        api_info
+    )
+    rpy_state = revolt.state.State(rpy_http, api_info, 1000)
+
+    def using_revoltpy():
+        return revolt.DMChannel(data=payload, state=rpy_state)
+
+    vpy_http = voltage.internals.http.HTTPHandler(session, '')
+    vpy_cache = voltage.internals.cache.CacheHandler(vpy_http, asyncio.get_event_loop())
+
+    def using_voltage():
+        return voltage.DMChannel(data=payload, cache=vpy_cache)
+
+    time_pyvolt = timeit.timeit(using_pyvolt,     number=100_000)
+    time_revoltpy = timeit.timeit(using_revoltpy, number=100_000)
+    time_voltage = timeit.timeit(using_voltage,   number=100_000)
+
+    print(f"[DMChannel] Time using pyvolt ----: {time_pyvolt:.6f} seconds")
+    print(f"[DMChannel] Time using revolt.py -: {time_revoltpy:.6f} seconds")
+    print(f"[DMChannel] Time using voltage ---: {time_voltage:.6f} seconds")
+
+    await session.close()
+
+async def bench_group_channels():
+    with open('./test/data/channels/group.json', 'r') as fp:
+        payload = json.load(fp)
+    
+    state = pyvolt.State()
+    parser = pyvolt.Parser(state)
+    state.setup(parser=parser)
+
+    def using_pyvolt():
+        return parser.parse_group_channel(payload, (True, payload['recipients']))
+
+    import aiohttp
+    session = aiohttp.ClientSession()
+
+    api_info: typing.Any = {
+        'features': {
+            'autumn': {
+                'url': 'https://autumn.revolt.chat/'
+            }
+        }
+    }
+    rpy_http = revolt.http.HttpClient(
+        session,
+        '',
+        'https://api.revolt.chat/',
+        api_info
+    )
+    rpy_state = revolt.state.State(rpy_http, api_info, 1000)
+
+    def using_revoltpy():
+        return revolt.GroupDMChannel(data=payload, state=rpy_state)
+
+    vpy_http = voltage.internals.http.HTTPHandler(session, '')
+    vpy_cache = voltage.internals.cache.CacheHandler(vpy_http, asyncio.get_event_loop())
+    
+    with open('./test/data/users/me.json', 'r') as fp:
+        me = json.load(fp)
+        vpy_cache.add_user(me)
+
+    with open('./test/data/users/mecha.json', 'r') as fp:
+        me = json.load(fp)
+        vpy_cache.add_user(me)
+
+
+    def using_voltage():
+        return voltage.GroupDMChannel(data=payload, cache=vpy_cache)
+
+    time_pyvolt = timeit.timeit(using_pyvolt,     number=100_000)
+    time_revoltpy = timeit.timeit(using_revoltpy, number=100_000)
+    time_voltage = timeit.timeit(using_voltage,   number=100_000)
+
+    print(f"[GroupChannel] Time using pyvolt ----: {time_pyvolt:.6f} seconds")
+    print(f"[GroupChannel] Time using revolt.py -: {time_revoltpy:.6f} seconds")
+    print(f"[GroupChannel] Time using voltage ---: {time_voltage:.6f} seconds")
+
+    await session.close()
+
+async def bench_text_channels():
     with open('./test/data/channels/rules_channel.json', 'r') as fp:
         payload = json.load(fp)
     
@@ -60,8 +165,8 @@ async def bench_channels():
     time_revoltpy = timeit.timeit(using_revoltpy, number=100_000)
     time_voltage = timeit.timeit(using_voltage,   number=100_000)
 
-    print(f"[User] Time using pyvolt ----: {time_pyvolt:.6f} seconds")
-    print(f"[User] Time using revolt.py -: {time_revoltpy:.6f} seconds")
-    print(f"[User] Time using voltage ---: {time_voltage:.6f} seconds")
+    print(f"[TextChannel] Time using pyvolt ----: {time_pyvolt:.6f} seconds")
+    print(f"[TextChannel] Time using revolt.py -: {time_revoltpy:.6f} seconds")
+    print(f"[TextChannel] Time using voltage ---: {time_voltage:.6f} seconds")
 
     await session.close()
