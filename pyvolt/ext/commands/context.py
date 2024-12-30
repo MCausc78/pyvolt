363c05ab7be5287c8053d96aaf52a672f90316d9
MCausc78 <mcausc78@gmail.com>
2024-12-30 21:56:49 +0200

    Just to merge

--- pyvolt/ext/commands/context.py
+++ pyvolt/ext/commands/context.py
@@ -10,44 +10,36 @@ if typing.TYPE_CHECKING:
 
 
 class Context(typing.Generic['BotT']):
-    r"""A class that implements the protocol for a bot text command.
+    r"""A invoking context for commands.
 
-    These are not created manually, instead they are created via the decorator or functional interface.
+    These are not created manually, instead they are created via :meth:`Bot.get_context` method.
 
     Attributes
     -----------
-    name: :class:`str`
-        The name of the command.
-    callback: :ref:`coroutine <coroutine>`
-        The coroutine that is executed when the command is called.
-    aliases: Union[List[:class:`str`], Tuple[:class:`str`]]
-        The list of aliases the command can be invoked under.
-    enabled: :class:`bool`
-        A boolean that indicates if the command is currently enabled.
-        If the command is invoked while it is disabled, then
-        :exc:`.DisabledCommand` is raised to the :func:`.on_command_error`
-        event. Defaults to ``True``.
-    checks: List[Callable[[:class:`.Context`], :class:`bool`]]
-        A list of predicates that verifies if the command could be executed
-        with the given :class:`.Context` as the sole parameter. If an exception
-        is necessary to be thrown to signal failure, then one inherited from
-        :exc:`.CommandError` should be used. Note that if the checks fail then
-        :exc:`.CheckFailure` exception is raised to the :func:`.on_command_error`
-        event.
-    description: :class:`str`
-        The message prefixed into the default help command.
-    hidden: :class:`bool`
-        If ``True``\, the default help command does not show this in the
-        help output.
-    rest_is_raw: :class:`bool`
-        If ``False`` and a keyword-only argument is provided then the keyword
-        only argument is stripped and handled as if it was a regular argument
-        that handles :exc:`.MissingRequiredArgument` and default values in a
-        regular matter rather than passing the rest completely raw. If ``True``
-        then the keyword-only argument will pass in the rest of the arguments
-        in a completely raw matter. Defaults to ``False``.
-    invoked_subcommand: Optional[:class:`.Command`]
-        The subcommand that was invoked, if any.
+    author: Union[:class:`pyvolt.Member`, :class:`pyvolt.User`]
+        The user who created this context.
+    author_id: :class:`str`
+        The user's ID who created this context.
+    bot: :class:`Bot`
+        The bot in this context.
+    channel: Union[:class:`pyvolt.TextableChannel`, :class:`pyvolt.PartialMessageable`]
+        The channel the context was created in.
+    command: Optional[:class:`.Command`]
+        The command used in this context.
+    label: :class:`str`
+        The substring used to invoke the command. May be empty sometimes.
+    me: Union[:class:`pyvolt.Member`, :class:`pyvolt.OwnUser`]
+        The bot user in this context.
+    message: :class:`pyvolt.Message`
+        The message that caused this context to be created.
+    prefix: :class:`str`
+        The prefix used to invoke command. May be empty sometimes.
+    server: Optional[:class:`pyvolt.Server`]
+        The server this command happened in.
+    shard: :class:`pyvolt.Shard`
+        The shard the context was created on.
+    view: :class:`.StringView`
+        The string view, used to parse command parameters.
     """
 
     __slots__ = (
